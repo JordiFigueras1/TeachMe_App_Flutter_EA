45bc4badb8115eb4746aa0a3cd83db68cc75acfd
julia <[julia.roquet@estudiantat.upc.edu]>
2024-12-31 04:39:03 +0100

    primera implementacion minimo2 julia, agregar imagen

--- lib/screen/perfil.dart
+++ lib/screen/perfil.dart
@@ -1,28 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/chat.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:html' as html; // Importación para interacción web
 import '../controllers/userListController.dart';
 import '../controllers/authController.dart';
 import '../controllers/connectedUsersController.dart';
 import '../controllers/socketController.dart';
-import '../screen/chat.dart';
 import '../controllers/theme_controller.dart';
+import '../helpers/image_picker_helper.dart';
+import '../services/cloudinary_service.dart';
 
-class PerfilPage extends StatelessWidget {
+class PerfilPage extends StatefulWidget {
+  @override
+  _PerfilPageState createState() => _PerfilPageState();
+}
+
+class _PerfilPageState extends State<PerfilPage> {
   final SocketController socketController = Get.find<SocketController>();
   final ThemeController themeController = Get.find<ThemeController>();
+  final userController = Get.find<UserListController>();
+  final authController = Get.find<AuthController>();
+  final connectedUsersController = Get.find<ConnectedUsersController>();
+  final ImagePickerHelper _imagePicker = ImagePickerHelper();
+  final CloudinaryService _cloudinaryService = CloudinaryService();
+
+  String? _profileImageUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProfileImageUrl();
+  }
+
+  Future<void> _loadProfileImageUrl() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _profileImageUrl = prefs.getString('profileImageUrl');
+    });
+  }
+
+  Future<void> _saveProfileImageUrl(String url) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('profileImageUrl', url);
+  }
+
+  Future<void> _selectAndUploadProfileImage() async {
+    final imageBase64 = await _imagePicker.pickImage();
+    if (imageBase64 != null) {
+      String? imageUrl = await _cloudinaryService.uploadImage(imageBase64);
+      if (imageUrl != null) {
+        setState(() {
+          _profileImageUrl = imageUrl;
+        });
+        _saveProfileImageUrl(imageUrl);
+      } else {
+        Get.snackbar('Error', 'No se pudo subir la imagen.');
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final userController = Get.find<UserListController>();
-    final authController = Get.find<AuthController>();
-    final connectedUsersController = Get.find<ConnectedUsersController>();
     final TextEditingController searchController = TextEditingController();
     final ThemeData theme = Theme.of(context);
 
     return Scaffold(
       backgroundColor: theme.scaffoldBackgroundColor,
       appBar: AppBar(
-        title: const Text('Buscar Usuarios'),
+        title: const Text('Perfil de Usuario'),
         backgroundColor: theme.appBarTheme.backgroundColor,
         elevation: 0,
         actions: [
@@ -41,10 +87,38 @@ class PerfilPage extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: _selectAndUploadProfileImage,
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: _profileImageUrl != null
+                        ? NetworkImage(_profileImageUrl!)
+                        : null,
+                    child: _profileImageUrl == null
+                        ? Icon(
+                            Icons.person,
+                            size: 50,
+                            color: theme.iconTheme.color,
+                          )
+                        : null,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _selectAndUploadProfileImage,
+                  child: const Text('Cambiar Foto de Perfil'),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
             child: TextField(
               controller: searchController,
               decoration: InputDecoration(
-                labelText: 'Nombre del Usuario',
+                labelText: 'Buscar Usuarios',
                 labelStyle: theme.textTheme.bodyLarge,
                 filled: true,
                 fillColor: theme.cardColor,
